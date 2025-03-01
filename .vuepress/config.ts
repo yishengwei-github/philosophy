7f379d681b58b728bb1a875eb768b9a8cc70c0a6
yshen <jayyi348694356@qq.com>
2025-03-01 22:05:43 +0800

    feat: Genesis

---
+++ .vuepress/config.ts
@@ -0,0 +1,51 @@
+import { defineUserConfig } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  title: "Know thyself",
+  description: "To Sophia",
+  bundler: viteBundler(),
+  // bundler: webpackBundler(),
+  theme: recoTheme({
+    logo: "/logo.png",
+    author: "yshen",
+    authorAvatar: "/head.png",
+    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "philosophy",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      "/docs/theme-reco/": [
+        {
+          text: "module one",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      { text: "Home", link: "/" },
+      { text: "Overview", link: "/docs/overview"},
+      { text: "Philosopher", link: "/docs/philosopher"},
+    ],
+    // commentConfig: {
+    //   type: 'valine',
+    //   // options 与 1.x 的 valineConfig 配置一致
+    //   options: {
+    //     // appId: 'xxx',
+    //     // appKey: 'xxx',
+    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
+    //     // verify: true, // 验证码服务
+    //     // notify: true,
+    //     // recordIP: true,
+    //     // hideComments: true // 隐藏评论
+    //   },
+    // },
+  }),
+  // debug: true,
+});
---
+++ README.md
@@ -0,0 +1,24 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - Blog
+  - MdContent
+  - Footer
+bannerBrand:
+  bgImage: '/bg.svg'
+  title: Know thyself
+  description: A Philosophy Lesson for My Daughter
+  tagline: To Sophia。
+  buttons:
+    - { text: Overview, link: '/docs/overview' }
+    - { text: Philosopher, link: '/docs/philosopher', type: 'plain' }
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco' }
+blog:
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/recoluan' }
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+---
